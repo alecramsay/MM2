8a7fca28bb45801b28d3a1b1a3cc86ee4430650a
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-08-08 15:38:10 -0700

    Resource links

--- MM2/congress.py
+++ MM2/congress.py
@@ -69,3 +69,25 @@ def assign_seat(election, verbose=False):
     party = Party.REP if (Sf > Vf) else Party.DEM
 
     return (party, Vf, Sf, PR, Df, scenario)
+
+
+### CONGRESSIONAL APPORTIONMENT ###
+
+"""
+See "Calculating Apportionment" in:
+https://www.census.gov/content/dam/Census/library/publications/2011/dec/c2010br-08.pdf
+
+The algorithm is the Huntington-Hill method:
+https://electionscience.org/library/congressional-apportionment-huntington-hill-method/
+https://en.wikipedia.org/wiki/Huntington%E2%80%93Hill_method
+
+Some supporting resources:
+https://www.census.gov/topics/public-sector/congressional-apportionment/about/computing.html
+https://support.ndsnfp.com/portal/en/community/topic/method-of-equal-proportions-from-the-u-s-census
+https://www2.census.gov/programs-surveys/decennial/2010/data/apportionment/PriorityValues2010.pdf
+
+There is implementation, but it's embedded in a general package and hard to understand & verify:
+https://pypi.org/project/apportionment/
+https://github.com/martinlackner/apportionment
+
+"""
