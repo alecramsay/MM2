32314c985d8907948178610d6397cb1db2ac5475
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-08-08 07:18:20 -0700

    Inspecting state results

--- add_list_seats.py
+++ add_list_seats.py
@@ -26,9 +26,30 @@ elections_by_state = read_typed_csv(csv_data, types)
 
 ### CALCULATE STATE & NATIONAL GAPS ###
 
-Vf, Sf, PR, gap = national_results(elections_by_state, verbose)
+nVf, nSf, nPR, nGap = national_results(elections_by_state, verbose)
 
-# - Calculate # proportional seats for each state
+# Inspect each state's results
+
+for state in elections_by_state:
+    Vf = state["DEM_V"] / (state["REP_V"] + state["DEM_V"])
+    Sf = state["DEM_S"] / (state["REP_S"] + state["DEM_S"])
+    PR = pr_seats(state["REP_S"] + state["DEM_S"], Vf)
+    gap = ue_seats(PR, state["DEM_S"])
+
+    scenario = None
+    if abs(gap) > 1:
+        scenario = "Case 1"
+    elif abs(gap) > 0.5:
+        scenario = "Case 2"
+    else:
+        scenario = "Case 3"
+
+    if verbose:
+        print(
+            "{}: Vf={:.2f}, Sf={:.2f}, PR={}, gap={}, case={}".format(
+                state["XX"], Vf, Sf, PR, gap, scenario
+            )
+        )
 
 
 ### REPLICATE 2010 REAPPORTIONMENT ###
