4283c8c223a4e353f2cbdf2f0af05002453e5385
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-08-10 08:39:07 -0700

    Replicating '90, '00, '10, and '20 apportionments

--- test/test_apportion.py
+++ test/test_apportion.py
@@ -4,6 +4,7 @@
 #
 
 from MM2 import *
+import pytest
 
 
 class TestApportioner:
@@ -18,25 +19,19 @@ class TestApportioner:
         assert app._priority_value(4802982, 2) == 3396221
         assert app._priority_value(4802982, 3) == 1960809
 
-    def test_assign_435(self):
-        # Replicate 2010 apportionment
-        csv_data = "data/census/{}_census.csv".format(2010)
+    @pytest.mark.parametrize("cycle", [1990, 2000, 2010, 2020])
+    def test_assign_435(self, cycle):
+        csv_data = "data/census/{}_census.csv".format(cycle)
         types = [str, str, int]
         census = read_typed_csv(csv_data, types)
 
         app = Apportioner(census)
         app.assign_435()
 
-        csv_data = "data/census/Reapportionment for {} Census.csv".format(2010)
+        csv_data = "data/census/Reapportionment for {} Census.csv".format(cycle)
         types = [str, str, int]
         reps_list = read_typed_csv(csv_data, types)
 
         for state in reps_list:
             xx = state["XX"]
             assert app.reps[xx] == state["REPS"]
-
-        # TODO: 2020
-
-        # TODO: 2000
-
-        # TODO: 1990
