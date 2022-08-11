0247ee9061dc7ed5d256a31495a9e883d0461b3c
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-08-11 08:30:37 -0700

    Done with #1

--- scripts/add_list_seats.py
+++ scripts/add_list_seats.py
@@ -6,7 +6,8 @@ Add list seats to base congressional apportionment for an election.
 
 For example:
 
-$ scripts/add_list_seats.py 2010 2012 -v
+$ scripts/add_list_seats.py 2000 2002 -v > results/2002_MM2_for_Congress.txt
+$ scripts/add_list_seats.py 2010 2012 -v > results/2012_MM2_for_Congress.txt
 
 For documentation, type:
 
