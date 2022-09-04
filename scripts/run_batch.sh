f6d354cd19861e6b259688e29d3ee3ab11537288
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-09-04 14:45:28 -0700

    Ran Strategy 2 for all elections

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -2,28 +2,21 @@
 #
 # Run MM2 for Congress script for many elections
 #
+# For example:
+#
+# scripts/run_batch.sh
+#
 
-scripts/MM2_for_Congress.py 1990 2000 -s 1
-scripts/MM2_for_Congress.py 1990 2000 -s 3
+scripts/MM2_for_Congress.py 1990 2000 -s 2
 
-scripts/MM2_for_Congress.py 2000 2002 -s 1
-scripts/MM2_for_Congress.py 2000 2002 -s 3
-scripts/MM2_for_Congress.py 2000 2004 -s 1
-scripts/MM2_for_Congress.py 2000 2004 -s 3
-scripts/MM2_for_Congress.py 2000 2006 -s 1
-scripts/MM2_for_Congress.py 2000 2006 -s 3
-scripts/MM2_for_Congress.py 2000 2008 -s 1
-scripts/MM2_for_Congress.py 2000 2008 -s 3
-scripts/MM2_for_Congress.py 2000 2010 -s 1
-scripts/MM2_for_Congress.py 2000 2010 -s 3
+scripts/MM2_for_Congress.py 2000 2002 -s 2
+scripts/MM2_for_Congress.py 2000 2004 -s 2
+scripts/MM2_for_Congress.py 2000 2006 -s 2
+scripts/MM2_for_Congress.py 2000 2008 -s 2
+scripts/MM2_for_Congress.py 2000 2010 -s 2
 
-scripts/MM2_for_Congress.py 2010 2012 -s 1
-scripts/MM2_for_Congress.py 2010 2012 -s 3
-scripts/MM2_for_Congress.py 2010 2014 -s 1
-scripts/MM2_for_Congress.py 2010 2014 -s 3
-scripts/MM2_for_Congress.py 2010 2016 -s 1
-scripts/MM2_for_Congress.py 2010 2016 -s 3
-scripts/MM2_for_Congress.py 2010 2018 -s 1
-scripts/MM2_for_Congress.py 2010 2018 -s 3
-scripts/MM2_for_Congress.py 2010 2020 -s 1
-scripts/MM2_for_Congress.py 2010 2020 -s 3
+scripts/MM2_for_Congress.py 2010 2012 -s 2
+scripts/MM2_for_Congress.py 2010 2014 -s 2
+scripts/MM2_for_Congress.py 2010 2016 -s 2
+scripts/MM2_for_Congress.py 2010 2018 -s 2
+scripts/MM2_for_Congress.py 2010 2020 -s 2
