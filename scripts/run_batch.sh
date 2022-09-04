d6804574afcecffc3f4ac64e78adbea18727f0b9
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-09-04 07:51:20 -0700

    Fixed 1990 elections (VT)

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -6,24 +6,24 @@
 scripts/MM2_for_Congress.py 1990 2000 -s 1
 scripts/MM2_for_Congress.py 1990 2000 -s 3
 
-scripts/MM2_for_Congress.py 2000 2002 -s 1
-scripts/MM2_for_Congress.py 2000 2002 -s 3
-scripts/MM2_for_Congress.py 2000 2004 -s 1
-scripts/MM2_for_Congress.py 2000 2004 -s 3
-scripts/MM2_for_Congress.py 2000 2006 -s 1
-scripts/MM2_for_Congress.py 2000 2006 -s 3
-scripts/MM2_for_Congress.py 2000 2008 -s 1
-scripts/MM2_for_Congress.py 2000 2008 -s 3
-scripts/MM2_for_Congress.py 2000 2010 -s 1
-scripts/MM2_for_Congress.py 2000 2010 -s 3
+# scripts/MM2_for_Congress.py 2000 2002 -s 1
+# scripts/MM2_for_Congress.py 2000 2002 -s 3
+# scripts/MM2_for_Congress.py 2000 2004 -s 1
+# scripts/MM2_for_Congress.py 2000 2004 -s 3
+# scripts/MM2_for_Congress.py 2000 2006 -s 1
+# scripts/MM2_for_Congress.py 2000 2006 -s 3
+# scripts/MM2_for_Congress.py 2000 2008 -s 1
+# scripts/MM2_for_Congress.py 2000 2008 -s 3
+# scripts/MM2_for_Congress.py 2000 2010 -s 1
+# scripts/MM2_for_Congress.py 2000 2010 -s 3
 
-scripts/MM2_for_Congress.py 2010 2012 -s 1
-scripts/MM2_for_Congress.py 2010 2012 -s 3
-scripts/MM2_for_Congress.py 2010 2014 -s 1
-scripts/MM2_for_Congress.py 2010 2014 -s 3
-scripts/MM2_for_Congress.py 2010 2016 -s 1
-scripts/MM2_for_Congress.py 2010 2016 -s 3
-scripts/MM2_for_Congress.py 2010 2018 -s 1
-scripts/MM2_for_Congress.py 2010 2018 -s 3
-scripts/MM2_for_Congress.py 2010 2020 -s 1
-scripts/MM2_for_Congress.py 2010 2020 -s 3
+# scripts/MM2_for_Congress.py 2010 2012 -s 1
+# scripts/MM2_for_Congress.py 2010 2012 -s 3
+# scripts/MM2_for_Congress.py 2010 2014 -s 1
+# scripts/MM2_for_Congress.py 2010 2014 -s 3
+# scripts/MM2_for_Congress.py 2010 2016 -s 1
+# scripts/MM2_for_Congress.py 2010 2016 -s 3
+# scripts/MM2_for_Congress.py 2010 2018 -s 1
+# scripts/MM2_for_Congress.py 2010 2018 -s 3
+# scripts/MM2_for_Congress.py 2010 2020 -s 1
+# scripts/MM2_for_Congress.py 2010 2020 -s 3
