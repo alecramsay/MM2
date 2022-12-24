b74eb8a35b1031dc55face950423537641afefe7
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-12-24 07:48:48 -0800

    Re-ran not-imputed w/ Strategy 8

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -7,11 +7,42 @@
 # scripts/run_batch.sh
 #
 
-scripts/MM2_for_Congress_LOCAL.py 2020 2022 -s 1
-scripts/MM2_for_Congress_LOCAL.py 2020 2022 -s 2
-scripts/MM2_for_Congress_LOCAL.py 2020 2022 -s 3
-scripts/MM2_for_Congress_LOCAL.py 2020 2022 -s 4
-scripts/MM2_for_Congress_LOCAL.py 2020 2022 -s 5
-scripts/MM2_for_Congress_LOCAL.py 2020 2022 -s 6
-scripts/MM2_for_Congress_LOCAL.py 2020 2022 -s 7
-scripts/MM2_for_Congress_LOCAL.py 2020 2022 -s 8
+echo "Running batch script"
+
+# echo "2000 ..."
+# scripts/MM2_for_Congress.py 1990 2000 -s 8 -r
+
+# echo "2002 ..."
+# scripts/MM2_for_Congress.py 2000 2002 -s 8 -r
+
+# echo "2004 ..."
+# scripts/MM2_for_Congress.py 2000 2004 -s 8 -r
+
+# echo "2006 ..."
+# scripts/MM2_for_Congress.py 2000 2006 -s 8 -r
+
+# echo "2008 ..."
+# scripts/MM2_for_Congress.py 2000 2008 -s 8 -r
+
+# echo "2010 ..."
+# scripts/MM2_for_Congress.py 2000 2010 -s 8 -r
+
+# echo "2012 ..."
+# scripts/MM2_for_Congress.py 2010 2012 -s 8 -r
+
+# echo "2014 ..."
+# scripts/MM2_for_Congress.py 2010 2014 -s 8 -r
+
+# echo "2016 ..."
+# scripts/MM2_for_Congress.py 2010 2016 -s 8 -r
+
+# echo "2018 ..."
+# scripts/MM2_for_Congress.py 2010 2018 -s 8 -r
+
+# echo "2020 ..."
+# scripts/MM2_for_Congress.py 2010 2020 -s 8 -r
+
+echo "2022 ..."
+scripts/MM2_for_Congress_LOCAL.py 2020 2022 -s 8 -r 
+
+echo "Done"
