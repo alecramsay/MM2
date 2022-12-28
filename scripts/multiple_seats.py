8e4061b19bd38a48cabde418350f6a755d112e80
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-12-28 15:21:48 -0800

    WIP

--- scripts/multiple_seats.py
+++ scripts/multiple_seats.py
@@ -2,84 +2,104 @@
 #
 
 """
-Q. How big does the House have to be for every state to have multiple seats?
-A. For the 2020 census, all states have multiple seats after 811 seats.
-
-   After 435 seats, 6 states have multiple seats: {'AK', 'DE', 'SD', 'ND', 'VT', 'WY'}
-   After 473 seats, 5 states have multiple seats: {'AK', 'SD', 'ND', 'VT', 'WY'}        <= 'DE'
-   After 526 seats, 4 states have multiple seats: {'AK', 'ND', 'VT', 'WY'}              <= 'SD'
-   After 601 seats, 3 states have multiple seats: {'AK', 'VT', 'WY'}                    <= 'ND'
-   After 641 seats, 2 states have multiple seats: {'VT', 'WY'}                          <= 'AK'
-   After 728 seats, 1 states have multiple seats: {'WY'}                                <= 'VT'
-   For the 2020 census, all states have multiple seats after 811 seats.                 <= 'WY'
+Analyze the # of seats per state by the total # of seats allocated.
 
 For example:
 
-$ scripts/multiple_seats.py | grep After
+$ scripts/multiple_seats.py 2020 | grep After
+
+For documentation, type:
+
+$ scripts/multiple_seats.py -h
 
 """
 
+import argparse
+from argparse import ArgumentParser, Namespace
 from typing import Tuple
 
 from MM2 import *
 
-
-cycle: int = 2020
-# verbose = True
+### PARSE ARGUMENTS ###
 
 
-### LOAD THE CENSUS ###
+def parse_args() -> Namespace:
+    parser: ArgumentParser = argparse.ArgumentParser(
+        description="Analyze # of seats per state for a census."
+    )
 
-csv_data: str = "data/census/{}_census.csv".format(cycle)
-types: list = [str, str, int]
-census: list = read_typed_csv(csv_data, types)
+    parser.add_argument(
+        "-c", "--cycle", default=2020, help="The census cycle (e.g., 2020)", type=int
+    )
+    parser.add_argument(
+        "-v", "--verbose", dest="verbose", action="store_true", help="Verbose mode"
+    )
+
+    args: Namespace = parser.parse_args()
+    return args
 
 
-### APPORTION NOMINAL SEATS UNTIL EVERY STATE HAS MORE THAN 1 ###
+def main() -> None:
+    args: Namespace = parse_args()
+    cycle: int = args.cycle
 
-app: HH_Apportioner = HH_Apportioner(census)
+    ### LOAD THE CENSUS ###
 
-for xx in STATES:
-    app.reps[xx] = 1
-app.N = 50
-single_seats: set[str] = set(STATES)
+    csv_data: str = "data/census/{}_census.csv".format(cycle)
+    types: list = [str, str, int]
+    census: list = read_typed_csv(csv_data, types)
 
-app._make_priority_queue()
+    ### APPORTION NOMINAL SEATS UNTIL EVERY STATE HAS MORE THAN 1 ###
 
-print()
+    app: HH_Apportioner = HH_Apportioner(census)
 
-while single_seats:
-    hs: int
-    pv: int
-    xx: str
-    ss: int
-    hs, pv, xx, ss = app.assign_next()
+    for xx in STATES:
+        app.reps[xx] = 1
+    app.N = 50
+    single_seats: set[str] = set(STATES)
 
-    single_seats.discard(xx)
+    app._make_priority_queue()
 
-    if app.N < 435:
-        continue
+    print()
 
-    if app.N == 435:
-        n_single_seats: int = len(single_seats)
-        print(
-            "After {} seats, {} states have multiple seats: {}".format(
-                app.N, len(single_seats), single_seats
-            )
-        )
-        continue
+    while single_seats:
+        hs: int
+        pv: int
+        xx: str
+        ss: int
+        hs, pv, xx, ss = app.assign_next()
 
-    if len(single_seats) < n_single_seats:
-        n_single_seats = len(single_seats)
-        if n_single_seats > 0:
+        single_seats.discard(xx)
+
+        if app.N < 435:
+            continue
+
+        if app.N == 435:
+            n_single_seats: int = len(single_seats)
             print(
                 "After {} seats, {} states have multiple seats: {}".format(
                     app.N, len(single_seats), single_seats
                 )
             )
+            continue
 
-print(
-    "For the {} census, all states have multiple seats after {} seats.".format(
-        cycle, app.N
+        if len(single_seats) < n_single_seats:
+            n_single_seats = len(single_seats)
+            if n_single_seats > 0:
+                print(
+                    "After {} seats, {} states have multiple seats: {}".format(
+                        app.N, len(single_seats), single_seats
+                    )
+                )
+
+    print(
+        "For the {} census, all states have multiple seats after {} seats.".format(
+            cycle, app.N
+        )
     )
-)
+
+
+if __name__ == "__main__":
+    main()
+
+pass
