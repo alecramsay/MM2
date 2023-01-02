dac6c5dae1888e2747a7f6ba0aecf0f26423e3a6
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-01-02 09:01:11 -0800

    WIP

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -7,20 +7,20 @@
 # scripts/run_batch.sh
 #
 
-echo "Running batch script"
+echo "Running batch script ..."
 
-scripts/apportion_seats.py --cycle 1990 -l 1
-scripts/apportion_seats.py --cycle 2000 -l 1
-scripts/apportion_seats.py --cycle 2000 -l 1
-scripts/apportion_seats.py --cycle 2020 -l 1
-scripts/apportion_seats.py --cycle 1990 -l 0
-scripts/apportion_seats.py --cycle 2000 -l 0
-scripts/apportion_seats.py --cycle 2000 -l 0
-scripts/apportion_seats.py --cycle 2020 -l 0
+# scripts/apportion_seats.py --cycle 1990 -l 1
+# scripts/apportion_seats.py --cycle 2000 -l 1
+# scripts/apportion_seats.py --cycle 2000 -l 1
+# scripts/apportion_seats.py --cycle 2020 -l 1
+# scripts/apportion_seats.py --cycle 1990 -l 0
+# scripts/apportion_seats.py --cycle 2000 -l 0
+# scripts/apportion_seats.py --cycle 2000 -l 0
+# scripts/apportion_seats.py --cycle 2020 -l 0
 
-# scripts/assign_seats_LOCAL.py -c 2020 -e 2022 -s 600 -l 0
-# scripts/assign_seats_LOCAL.py -c 2020 -e 2022 -s 600 -l 1
-# scripts/assign_seats_LOCAL.py -c 2020 -e 2022 -s 650 -l 0
-# scripts/assign_seats_LOCAL.py -c 2020 -e 2022 -s 650 -l 1
+scripts/assign_seats_LOCAL.py -c 2020 -e 2022 -s 600 -l 0
+scripts/assign_seats_LOCAL.py -c 2020 -e 2022 -s 600 -l 1
+scripts/assign_seats_LOCAL.py -c 2020 -e 2022 -s 650 -l 0
+scripts/assign_seats_LOCAL.py -c 2020 -e 2022 -s 650 -l 1
 
-echo "Done"
+echo "... done"
