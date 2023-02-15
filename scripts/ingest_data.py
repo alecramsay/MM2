77b5c740ec0c0255f495a5b3cbe649252e1319da
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-02-15 08:13:18 -0800

    Fleshed out ingest_data.py script

--- scripts/ingest_data.py
+++ scripts/ingest_data.py
@@ -14,32 +14,78 @@ from MM2 import *
 
 rawdata: str = "data/census/intake/congress_elections_imputations_2023.csv"
 years: List[int] = list(range(1972, 2022 + 1, 2))
-csv_dir: str = "data/census/"
+csv_dir: str = "data/elections/"
 
 
 def main() -> None:
+    # Initialize accumulators
 
-    print("Do something!")  # TODO
-
-    # Initialize accumulators for a state - a dict
-    # Initialize accumulators for all states - a dict of dicts
-    # Initialize accumulators for all elections - a dict of dicts of dicts (1972 - 2022)
+    state_bins: dict[str, int] = {
+        "REP_V": 0,
+        "DEM_V": 0,
+        "OTH_V": 0,
+        "TOT_V": 0,
+        "REP_S": 0,
+        "DEM_S": 0,
+        "OTH_S": 0,
+        "TOT_S": 0,
+    }
+    election_bins: dict = dict()
+    for xx in STATES:
+        election_bins[xx] = state_bins.copy()
+        election_bins[xx]["STATE"] = STATE_NAMES[xx]
+    data_bins: dict = dict()
+    for year in years:
+        data_bins[year] = election_bins.copy()
 
     # Read the CSV and accumulate the data by state and election (year)
+
+    print("TODO - Read the CSV and accumulate the data by state and election (year)")
+    # TODO - read_typed_csv(rel_path, field_types)
+
+    # Accumulate the data by state, election (year), and field
+
+    print("TODO - Accumulate the data by state, election (year), and field")
 
     # Convert the accumulated data to a list of list of dicts -- a list of elections,
     #   where each election is a list of dicts for each state.
 
     elections: list = list()
     for year in years:
-        election: list = list()
+        rows: list = list()
+        for key, value in data_bins[year].items():
+            row: dict = {"YEAR": year, "XX": key}
+            row.update(value)
+            # TODO - Calculate VOTE_% and SEAT_% here
 
-        print(f"Flatten the dict of dicts into a list of dicts for {year}")
+            rows.append(row)
+        elections.append(rows)
 
     # Write each election to a CSV
 
+    cols: list[str] = [
+        "YEAR",
+        "STATE",
+        "XX",
+        "REP_V",
+        "DEM_V",
+        "OTH_V",
+        "TOT_V",
+        "REP_S",
+        "DEM_S",
+        "OTH_S",
+        "TOT_S",
+        "VOTE_%",
+        "SEAT_%",
+    ]
+
     for i, year in enumerate(years):
-        print(f"Write {year} to CSV")
+        name: str = f"Congressional Elections ({year}).csv"
+        rel_path: str = csv_dir + name
+        rows: list = elections[i]
+
+        print(f"Write '{name}' to CSV")
+        # TODO -  write_csv(rel_path, rows, cols)
 
     pass
 
