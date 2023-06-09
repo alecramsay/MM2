850a17848d378db54a0a2bde5c37a163418dc700
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-06-09 14:23:31 -0700

    WIP

--- MM2/__init__.py
+++ MM2/__init__.py
@@ -4,6 +4,7 @@ from .congress import *
 from .explore import *
 from .reports import *
 from .apportion import *
+from .dhondt import *
 from .analytics import *
 from .readwrite import *
 from .constants import *
---
+++ MM2/dhondt.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#
+# The D'Hondt method of apportionment
+#
+
+import math
+
+x = 1
+
+
+class DHondt_Apportioner:
+    """
+    https://en.wikipedia.org/wiki/D%27Hondt_method
+    """
+
+    pass
